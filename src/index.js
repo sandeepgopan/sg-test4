0cc24d250007097be82e2384490a35ec12c20187
Sandeep Gopan <sandeepgopan111@gmail.com>
2021-10-18 23:07:39 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import "./styles.css";
 let incrCntr = document.querySelector("#incr-cntr");
 document.querySelector("#btn-incrmt").addEventListener("click", () => {
-  document.getElementById("incr-cntr").style.color = "red";
+  document.getElementById("incr-cntr").innerHTML = "Sandeep";
+  document.getElementById("incr-cntr1").innerHTML = "Sandeep";
+  document.getElementById("incr-cntr2").innerHTML = "Sandeep";
 });
 document.querySelector("#btn-minus").addEventListener("click", () => {
   document.getElementById("incr-cntr").style.fontSize = "200px";
